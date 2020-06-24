70763e0444fb0cab35d0d24e9d1d2b257557f69c
metrayim <metrayim@gmail.com>
2020-06-24 10:29:35 +0700

    add in file DemoApplication.java

--- src/main/java/com/jenkiess/demo/DemoApplication.java
+++ src/main/java/com/jenkiess/demo/DemoApplication.java
@@ -2,10 +2,16 @@ package com.jenkiess.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
+	@GetMapping("/api")
+	public String helloWorld(){
+		return "me me me ";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
